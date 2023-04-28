58b1807bf6cf73261f115a903ec238d4c8ccfd04
https://github.com/43693962/43693962 <adricuestas123@gmail.com>
2023-04-28 09:49:57 -0300

    ejercicios js

---
+++ main.js
@@ -0,0 +1,28 @@
+// Ejercicios JAVASCRIPT
+
+let miNombre = 'Adrian';    //ejercicio 1
+let miApellido = 'Cuestas'; //ejercicio 2
+let miEdad = 21;            //ejercicio 3
+let miMascota = 'Dona';     //ejercicio 4
+let edadMascota = 14;       //ejercicio 5 
+
+console.log(miNombre, miApellido, miEdad, miMascota, edadMascota); //ejercicio 6
+
+let nombreCompleto = "miNombre" + ' ' + "miApellido" //ejercicio 7
+
+const textoPresentacion = 'Hola  mi nombre es ${ nombreCompleto } y tengo ${miEdad}años . Y mi perra se llama ${miMascota} de ${edadMascota} años' 
+console.log(textoPresentacion) //ejercicio 8
+
+let sumaEdades= "miEdad" + "edadMascota"
+console.log= (sumaEdades)
+let restaEdades = "miEdad" - "edadMascota"
+console.log=(restaEdades)                        
+let productoEdades= "miEdad" *  "edadMascota"
+console.log=(productoEdades)
+let divisionEdades= "miEdad" / "edadMascota"
+console.log=(divisionEdades) //ejercicio 9
+
+
+let presentacionCompleta = `${textoPresentacion}, nuestras edades suman ${sumaEdades}, 
+restan ${restaEdades}, multiplican ${productoEdades} y por último dividen ${divisionEdades}.`
+console.log(presentacionCompleta); //ejercicio 10
